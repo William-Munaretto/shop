88696a373a2cd48d1aa1088249aa9cf9ceece795
William-Munaretto <william.munaretto@gmail.com>
2025-01-14 16:39:24 -0300

    criada pagina geral de produtos - products_overview_page.dart

---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shop/components/product_item.dart';
+import 'package:shop/models/product.dart';
+import '../data/dummy_data.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  ProductsOverviewPage({super.key});
+  final List<Product> loadedProducts = dummyProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (ctx, index) {
+          return ProductItem(product: loadedProducts[index]);
+        },
+      ),
+    );
+  }
+}
