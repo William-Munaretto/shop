5d836c27ad0b9aa08420014a25004098683707f9
William-Munaretto <william.munaretto@gmail.com>
2025-01-25 12:16:20 -0300

    criado provider para productList

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:shop/pages/counter_page.dart';
-import 'package:shop/pages/product_detail_page.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/products_overview_page.dart';
-import 'package:shop/providers/counter.dart';
-import 'package:shop/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CounterProvider(
+    return ChangeNotifierProvider(
+      create: (_) => ProductList(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
@@ -24,9 +23,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Lato',
         ),
         home: ProductsOverviewPage(),
-        routes: {
-          AppRoutes.PRODUCT_DETAIL: (ctx) => const CounterPage(),
-        },
+        routes: {},
       ),
     );
   }
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,31 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/models/product.dart';
-import '../data/dummy_data.dart';
+import 'package:shop/components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
-  ProductsOverviewPage({super.key});
-  final List<Product> loadedProducts = dummyProducts;
+  const ProductsOverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Minha Loja'),
+        title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (ctx, index) {
-          return ProductItem(product: loadedProducts[index]);
-        },
-      ),
+      body: const ProductGrid(),
     );
   }
 }
