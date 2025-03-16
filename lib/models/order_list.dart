2a8e67809da465ab8d3db2409d284aa565c7cf9b
William-Munaretto <william.munaretto@gmail.com>
2025-03-16 15:29:35 -0300

    criado o furute dos pedidos e finalizada a seção

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 import 'package:shop/models/order.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop/utils/contants.dart';
@@ -46,6 +47,32 @@ class OrderList with ChangeNotifier {
         date: DateTime.now(),
       ),
     );
+    notifyListeners();
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response = await http.get(Uri.parse('${Constants.ORDER_BASE_URL}.json'));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((ordertId, orderData) {
+      _items.add(
+        Order(
+          id: ordertId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
     notifyListeners();
   }
 }
